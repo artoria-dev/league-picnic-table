935f8cb22426791d6766443e5e38245cfe4cbf75
artoria-dev <tomjaenicke21@gmail.com>
2024-05-07 13:44:58 +0200

    added stuff

---
+++ champ_class/translate.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get(f"https://ddragon.leagueoflegends.com/cdn/14.9.1/data/en_US/champion.json")
+c_data = response.json()
+c_dict = {}
+for champion in c_data['data']:
+    c_dict[c_data['data'][champion]['name']] = c_data['data'][champion]['tags'][0]
+for key in c_dict:
+    print(f'"{key}": "{c_dict[key]}",')
